4452869dbe86320b87f605e6e3984008bb500517
Tarana <75437852+taranahassan@users.noreply.github.com>
2021-02-08 09:50:57 -0500

    Schema image and code for table structure.

--- Schema.sql
+++ Schema.sql
@@ -8,6 +8,7 @@ CREATE TABLE departments (
 
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
+	birth_date DATE NOT NULL,
 	first_name VARCHAR NOT NULL,
 	last_name VARCHAR NOT NULL,
 	gender VARCHAR NOT NULL,
@@ -25,30 +26,30 @@ CREATE TABLE dept_manager (
     PRIMARY KEY (emp_no, dept_no)
 );
 
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no, from_date)
+);
+
+CREATE TABLE  titles (
+	emp_no INT NOT NULL,
+	title VARCHAR(40) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, title)
+);
+
 CREATE TABLE salaries (
   	emp_no INT NOT NULL,
 	salary INT NOT NULL,
   	from_date DATE NOT NULL,
   	to_date DATE NOT NULL,
   	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
-);
-
-CREATE TABLE dept_emp (
-	emp_no INT NOT NULL,
-	dept_no VARCHAR NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no, dept_no)
-);
-
-CREATE TABLE  titles (
-	emp_no INT NOT NULL,
-	title VARCHAR NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no, title)
+  	PRIMARY KEY (emp_no, salary)
 );
